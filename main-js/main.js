181a248efdbc1592052a59ecb037bda36e465a6f
petskungu <pngumi25@gmail.com>
2024-09-18 10:03:05 +0300

    Update main.js

--- main-js/main.js
+++ main-js/main.js
@@ -145,3 +145,31 @@ document.getElementById('emailForm').addEventListener('submit', (event) => {
     console.log('Email submitted:', email); // Process the email (e.g., send it to the server)
     closeModal(); // Close the modal
 });
+
+var modal = document.getElementById("videoModal");
+var video = document.getElementById("modalVideo");
+
+// Get the button that opens the modal
+var btn = document.getElementById("openVideoModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+    video.pause();  // Pause the video when the modal is closed
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        video.pause();  // Pause the video when the modal is closed
+    }
+}
